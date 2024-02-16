ee8c02412140d9cf6611fdebe9152d8ee5b14a6d
Leon Gruber <123667404+9zehn@users.noreply.github.com>
2024-02-16 16:48:22 -0500

    Update curse.py

--- curse.py
+++ curse.py
@@ -10,12 +10,11 @@ line_styles = {0: 'ro-', 1: 'b^-', 2: 'gs-', 3: 'cv-'}
 # Plot dmax/dmin ratio
 # TODO: fill in valid test numbers
 for idx, num_samples in enumerate([750,1500,2250,3000]):
-    # TODO: Fill in a valid feature range
     feature_range = list(range(1,101))
     ratios = []
 
     for num_features in feature_range:
-        # TODO: Generate synthetic data using make_classification
+        # Generate synthetic data
         X,_ = make_classification(n_samples=num_samples,
                                 n_features=num_features,
                                 n_informative=num_features,
@@ -24,16 +23,14 @@ for idx, num_samples in enumerate([750,1500,2250,3000]):
                                 n_redundant=0,
                                 n_clusters_per_class=1)
 
-        # TODO: Choose random query point from X
+        # Choose random query point from X
         query_index = np.random.randint(0,len(X))
         query_point = X[query_index]
 
         remove_point = np.argwhere(np.all(X==query_point,axis=1))
         X_temp = np.delete(X,remove_point,axis=0)
 
-        # TODO: remove query pt from X so it isn't used in distance calculations
-
-        # TODO: Calculate distances
+        # Calculate distances
         delta = X_temp - query_point
         temp_result = np.sum(delta**2, axis = 1)
 
